ca127d2f6f57ba5a4481e9ecf191e5f1c79a8a9b
Omar Ayman <se.omarayman@gmail.com>
2022-11-14 20:02:21 +0200

    formatting

--- discs/disc06/q1-1.py
+++ discs/disc06/q1-1.py
@@ -1,20 +1,25 @@
 class Student:
-    students = 0 # this is a class attribute
+    students = 0  # this is a class attribute
+
     def __init__(self, name, ta):
-        self.name = name # this is an instance attribute
+        self.name = name  # this is an instance attribute
         self.understanding = 0
         Student.students += 1
         print("There are now", Student.students, "students")
         ta.add_student(self)
+
     def visit_office_hours(self, staff):
         staff.assist(self)
         print("Thanks, " + staff.name)
-        
+
+
 class Professor:
-    def __init__( self, name):
+    def __init__(self, name):
         self.name = name
         self.students = {}
+
     def add_student(self, student):
         self.students[student.name] = student
+
     def assist(self, student):
         student.understanding += 1
--- discs/disc06/q1-2.py
+++ discs/disc06/q1-2.py
@@ -2,22 +2,27 @@
 # email. Fill in the definitions below to finish the implementation! There are more
 # methods to fill out on the next page.
 
+
 class Email:
     """Every email object has 3 instance attributes: the
     message, the sender name, and the recipient name.
     """
+
     def __init__(self, msg, sender_name, recipient_name):
         self.msg = msg
         self.sender_name = sender_name
         self.recipient_name = recipient_name
+
 
 class Server:
     """Each Server has an instance attribute clients, which
     is a dictionary that associates client names with
     client objects.
     """
+
     def __init__(self):
         self.clients = {}
+
     def send(self, email):
         """Take an email and put it in the inbox of the client
         it is addressed to.
@@ -31,12 +36,14 @@ class Server:
         """
         self.clients[client_name] = client
 
+
 class Client:
     """Every Client has instance attributes name (which is
     used for addressing emails to the client), server
     (which is used to send emails out to other clients), and
     inbox (a list of all emails the client has received).
     """
+
     def __init__(self, server, name):
         self.inbox = []
         self.name = name
