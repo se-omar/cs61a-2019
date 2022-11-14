d771649e083fd25755c22370bccc09c7e9f7b26d
Omar Ayman <se.omarayman@gmail.com>
2022-11-14 19:10:17 +0200

    finished disc06 problem 1.2

---
+++ discs/disc06/q1-2.py
@@ -0,0 +1,55 @@
+# 1.2 We now want to write three different classes, Server, Client, and Email to simulate
+# email. Fill in the definitions below to finish the implementation! There are more
+# methods to fill out on the next page.
+
+class Email:
+    """Every email object has 3 instance attributes: the
+    message, the sender name, and the recipient name.
+    """
+    def __init__(self, msg, sender_name, recipient_name):
+        self.msg = msg
+        self.sender_name = sender_name
+        self.recipient_name = recipient_name
+
+class Server:
+    """Each Server has an instance attribute clients, which
+    is a dictionary that associates client names with
+    client objects.
+    """
+    def __init__(self):
+        self.clients = {}
+    def send(self, email):
+        """Take an email and put it in the inbox of the client
+        it is addressed to.
+        """
+        client = self.clients[email.recipient_name]
+        client.recieve(email)
+
+    def register_client(self, client, client_name):
+        """Takes a client object and client_name and adds it
+        to the clients instance attribute.
+        """
+        self.clients[client_name] = client
+
+class Client:
+    """Every Client has instance attributes name (which is
+    used for addressing emails to the client), server
+    (which is used to send emails out to other clients), and
+    inbox (a list of all emails the client has received).
+    """
+    def __init__(self, server, name):
+        self.inbox = []
+        self.name = name
+        self.server = server
+
+    def compose(self, msg, recipient_name):
+        """Send an email with the given message msg to the
+        given recipient client.
+        """
+        return Email(msg, self, recipient_name)
+
+    def receive(self, email):
+        """Take an email and add it to the inbox of this
+        client.
+        """
+        self.inbox.append(email)
