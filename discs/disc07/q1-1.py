de0ca12c3ca85d5c0b14be3a4f961dc9171b6bcc
Omar Ayman <se.omarayman@gmail.com>
2022-12-14 15:37:37 +0200

    finished q1-1 disc07

---
+++ discs/disc07/q1-1.py
@@ -0,0 +1,39 @@
+from link import Link
+# 1.1 Write a function that takes in a Python list of linked lists and multiplies them
+# element-wise. It should return a new linked list.
+# If not all of the Link objects are of equal length, return a linked list whose length is
+# that of the shortest linked list given. You may assume the Link objects are shallow
+# linked lists, and that lst of lnks contains at least one linked list.
+def multiply_lnks(lst_of_lnks):
+    """
+    >>> a = Link(2, Link(3, Link(5)))
+    >>> b = Link(6, Link(4, Link(2)))
+    >>> c = Link(4, Link(1, Link(0, Link(2))))
+    >>> p = multiply_lnks([a, b, c])
+    >>> p.first
+    48
+    >>> p.rest.first
+    12
+    >>> p.rest.rest.rest is Link.empty
+    True
+    """
+    firsts = 1
+    lnk_of_rests = []
+    for lnk in lst_of_lnks:
+        if lnk is Link.empty:
+            return Link(Link.empty)
+
+    for lnk in lst_of_lnks:
+        firsts *= lnk.first
+        lnk_of_rests.append(lnk.rest)
+
+    return Link(firsts, multiply_lnks(lnk_of_rests))
+
+
+a = Link(2, Link(3, Link(5)))
+b = Link(6, Link(4, Link(2)))
+c = Link(4, Link(1, Link(0, Link(2))))
+p = multiply_lnks([a, b, c])
+print(p.first)
+print(p.rest.first)
+print(p.rest.rest.rest)
