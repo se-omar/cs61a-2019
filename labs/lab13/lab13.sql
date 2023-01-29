6aca7ec28101d655910d1b57019a56167625a0e2
Omar Ayman <se.omarayman@gmail.com>
2023-01-29 14:23:39 +0200

    finished q03 lab13

--- labs/lab13/lab13.sql
+++ labs/lab13/lab13.sql
@@ -4,7 +4,10 @@ CREATE TABLE obedience AS
   SELECT seven, instructor from students;
 
 CREATE TABLE smallest_int AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT time, smallest from students
+  where smallest > 2
+  order by smallest
+  limit 20;
 
 CREATE TABLE matchmaker AS
   SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
