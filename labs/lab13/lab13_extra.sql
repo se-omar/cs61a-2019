b8d5a63b2c153b3f0a5d794eea705b5976c5590f
Omar Ayman <se.omarayman@gmail.com>
2023-01-29 14:56:34 +0200

    finished lab13

--- labs/lab13/lab13_extra.sql
+++ labs/lab13/lab13_extra.sql
@@ -1,12 +1,15 @@
 .read lab13.sql
 
 CREATE TABLE su19favpets AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT pet, count(*) as count from students group by pet order by count desc, pet limit 10;
 
 
 CREATE TABLE su19dog AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT pet, count(*) as count from students where pet = "dog" group by pet;
 
 
 CREATE TABLE obedienceimages AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT seven, instructor, count(*) as count
+  from students 
+  where seven = '7'
+  group by instructor;
